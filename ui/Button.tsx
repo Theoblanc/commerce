469b83979f369272391a92da8258105a6f9dd916
theoblanc <jtho1015@gmail.com>
2023-04-18 17:01:45 +0900

    feat: implement button

--- ui/Button.tsx
+++ ui/Button.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import classNames from "classnames";
 import { PropsWithChildren, useId } from "react";
 
 type ButtonProps = PropsWithChildren<{
   type?: "primary" | "danger" | "light" | "dark";
-  style?: "fill" | "outline" | "weak" | "flat";
-  display?: "inline" | "block" | "full";
+  full?: boolean;
+  size?: "xs" | "s";
+  textColor?: "primary" | "light";
 
   className?: string;
   onClick?: React.MouseEventHandler<HTMLElement>;
@@ -11,17 +15,52 @@ type ButtonProps = PropsWithChildren<{
 }>;
 
 export default function Button(props: ButtonProps) {
-  const { type, style, display, disabled, onClick, children } = props;
+  const {
+    type,
+    size,
+    full,
+    textColor,
+    className,
+    disabled,
+    onClick,
+    children,
+  } = props;
   const buttonId = useId();
 
   return (
     <button
       id={buttonId}
-      className={`${type} ${display}`}
+      className={classNames(
+        {
+          [`bg-${type}`]: type ? true : false,
+          [`w-full`]: full,
+        },
+        "h-12",
+        "rounded-xl",
+        "focus:outline-none border",
+        "border-gray-200",
+        "hover:bg-gray-100",
+        "focus:z-10",
+        "focus:ring-1",
+        "font-medium",
+        "focus:ring-gray-200",
+        "group",
+        className
+      )}
       disabled={disabled}
       onClick={onClick}
     >
-      <span>{children}</span>
+      <span
+        className={classNames(
+          {
+            [`text-${textColor}`]: textColor ? true : false,
+            [`text-${size}`]: size,
+          },
+          "group-hover:text-dark"
+        )}
+      >
+        {children}
+      </span>
     </button>
   );
 }
